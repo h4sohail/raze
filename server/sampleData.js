934973cc13a59360ed7899dcf5b30f8f4f445187
Hamza <hamza.sohail@carleton.ca>
2020-09-01 17:00:13 -0400

    Update sampleData.js

--- server/sampleData.js
+++ server/sampleData.js
@@ -1,9 +1,5 @@
 ﻿let sampleData = {};
 
-// This class holds sample data used by some generated pages to show how they can be used.
-// TODO Web Template Studio: Delete this file once your app is using real data.
-// TODO Web Template Studio: If you use a database replace this ID with the ID created by the database
-
 sampleData = {
   'account': {
     'id': 'nullptr#0001',
